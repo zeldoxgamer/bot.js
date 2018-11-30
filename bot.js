6a7b4240b1091b6f539629a987d1e4a2786e52d5
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2018-11-30 22:15:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -237,49 +237,10 @@ client.on("message", message => {
         
     
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("461567758518452247");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.cod
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
 
 
 
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("461584042547085313");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("461567758518452247");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
+
 
  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
   let banse = new Set();
@@ -399,58 +360,9 @@ client.on("guildMemberAdd", (member) => {
   })
 
 
-let ar = JSON.parse(fs.readFileSync(`AutoRole.json`, `utf8`))
-client.on('message', message => {
-  var sender = message.author
- 
-if(!message.guild) return
-  if(!ar[message.guild.id]) ar[message.guild.id] = {
-  onoff: 'On',
-  role: '"EverYone»'
-  }
- 
-if(message.content.startsWith(`+autorole`)) {
-         
-  let perms = message.member.hasPermission(`MANAGE_ROLES`)
- 
-  if(!perms) return message.reply(`You don't have permissions, required permission : Manage Roles.`)
- let args = message.content.split(" ").slice(1)
- if(!args.join(" ")) return message.reply(`${prefix}autorole toggle / set [ROLE NAME]`)
- let state = args[0]
- if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'setrole') return message.reply(`Please type a right state, ${prefix}modlogs toggle/setrole [ROLE NAME]`)
-   if(state.trim().toLowerCase() == 'toggle') {
-    if(ar[message.guild.id].onoff === 'Off') return [message.channel.send(`**The Autorole Is __𝐎𝐍__ !**`), ar[message.guild.id].onoff = 'On']
-    if(ar[message.guild.id].onoff === 'On') return [message.channel.send(`**The Autorole Is __𝐎𝐅𝐅__ !**`), ar[message.guild.id].onoff = 'Off']
-   }
-  if(state.trim().toLowerCase() == 'set') {
-  let newRole = message.content.split(" ").slice(2).join(" ")
-  if(!newRole) return message.reply(`${prefix}autorole set [ROLE NAME]`)
-    if(!message.guild.roles.find(`name`,newRole)) return message.reply(`I Cant Find This Role.`)
-   ar[message.guild.id].role = newRole
-    message.channel.send(`**The AutoRole Has Been Changed to ${newRole}.**`)
-  }
-        }
-if(message.content === '+info') {
-   let perms = message.member.hasPermission(`MANAGE_GUILD`)
-   if(!perms) return message.reply(`You don't have permissions.`)
-    var embed = new Discord.RichEmbed()
- 
-.addField(`Autorole : :sparkles:  `, `
-State : __${ar[message.guild.id].onoff}__
-Role : __${ar[message.guild.id].role}__`)
+
  
  
-    .setColor(`BLUE`)
-    message.channel.send({embed})
-  }
- 
- 
-    fs.writeFile("./AutoRole.json", JSON.stringify(ar), (err) => {
-    if (err) console.error(err)
-  });
- 
- 
-});
 
 
 client.on('message',async message => {
