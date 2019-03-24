e702b57ab135cdbb620955b41a4e10c9527361fd
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-03-24 21:33:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,49 @@ client.on('ready', () => {
  let prefix = '+'; 
 
 
+client.on("guildMemberAdd", member => {
+  let welcomer = member.guild.channels.find("name","welcome");
+        if(!welcomer) return;
+        if(welcomer) {
+           moment.locale('ar-ly');
+           var h = member.user;
+          let norelden = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setThumbnail(h.avatarURL)
+          .setAuthor(h.username,h.avatarURL)
+          .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **n** `${moment(member.user.createdAt).fromNow()}``,true)            
+           .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} n``${moment(member.joinedAt).startOf(' ').fromNow()}```, true) 
+           .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+       welcomer.send({embed:norelden});          
+                 
+   
+        }
+        });
+		
+
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+client.on('ready', () => {
+  wait(1000);
+  client.guilds.forEach(king => {
+    king.fetchInvites().then(guildInvites => {
+      invites[king.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const gamer = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => gamer.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const welcome = member.guild.channels.find(channel => channel.name === "welcome");
+    welcome.send(` ||${member.user.tag}|| invited by ||${inviter.tag}|| invites =  ||${invite.uses}|| `)
+  });
+});
+
+
 
 client.on('message',message =>{
     var prefix = "+";
