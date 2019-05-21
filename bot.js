34208bf0dc8e18a59d349d8bdc1f9a15645cae0c
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-05-21 18:20:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,17 @@ const Discord = require('discord.js');
 const fs = require('file-system');
 const client = new Discord.Client();
 
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
+
+
+
+
 client.on('ready', () => {
 
   client.user.setGame (`+help | +inv | ser ${client.guilds.size}| user ${client.users.size}`,'https://www.twitch.tv/peery13');
